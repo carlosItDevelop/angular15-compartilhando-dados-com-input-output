cb5cd9126bb5c932f4eb116ca80d646fa40f01b6
Carlos A Santos <carlos.itdeveloper@gmail.com>
2023-01-14 10:15:31 -0300

    lettle changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DodasEntreComponentes';
+[x: string]: any;
+
+  listaUsuarios = [
+    { id: 1, nome: 'Alexandre' },
+    { id: 2, nome: 'Ana' },
+    { id: 3, nome: 'Anna' },
+    { id: 4, nome: 'Thiago' },
+    { id: 5, nome: 'Eduardo' },
+    { id: 6, nome: 'Junior' }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GaleriaComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
